73716c65f026da2f65dfdb2e64493562c269e424
Masa H <masamasa0669@gmail.com>
2022-01-20 02:04:38 +0900

    create GUI

--- GUI.py
+++ GUI.py
@@ -1,8 +1,24 @@
 import tkinter as tk
+import tkinter.messagebox as mb
+
 
 root = tk.Tk()
+root.geometry('300x200')
+root.title('作業アサインシート一括印刷')
 
-button = tk.Button(root, text = 'Hello, Python3/Tkinter!!')
-button.pack()
+label1 = tk.Label(text='機番')
+label2 = tk.Label(text='エチロン')
+
+label1.place(x=30, y=20)
+label2.place(x=30, y=80)
+
+
+txt1 = tk.Entry(width=20)
+txt2 = tk.Entry(width=20)
+
+
+txt1.place(x=30, y=45)
+txt2.place(x=30, y=105)
+
 
 root.mainloop()
