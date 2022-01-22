499db323809ba9492e04b93b2ac37d137c30c24b
Masa H <masamasa0669@gmail.com>
2022-01-22 19:55:38 +0900

    GUIのレイアウト変更　excelファイル作成→保存成功

--- GUI.py
+++ GUI.py
@@ -1,24 +1,47 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 import tkinter.messagebox as mb
+from tkinter import messagebox
+
+import openpyxl as op
 
 
-root = tk.Tk()
-root.geometry('300x200')
-root.title('作業アサインシート一括印刷')
-
-label1 = tk.Label(text='機番')
-label2 = tk.Label(text='エチロン')
-
-label1.place(x=30, y=20)
-label2.place(x=30, y=80)
+def create():
+  wb = op.Workbook()
+  sheet = wb.active
+  wb.save('Ship Data.xlsx')
+  shiptext_value=ship_text.get()
+  echtext_value=ech_text.get()
+  messagebox.showinfo("完了", "完了しました")
 
 
-txt1 = tk.Entry(width=20)
-txt2 = tk.Entry(width=20)
+main_win = tk.Tk()
+main_win.geometry('300x200')
+main_win.title('作業アサインシート一括印刷')
 
+main_frame = ttk.Frame(main_win)
+main_frame.grid(column=0, row=0, sticky=tk.NSEW, padx=20, pady=20)
 
-txt1.place(x=30, y=45)
-txt2.place(x=30, y=105)
+ship_label = tk.Label(main_frame, text='機番 (JAは不要)')
+ship_text = tk.StringVar()
+shiptext_entry = tk.Entry(main_frame, textvariable=ship_text)
 
+ech_label = tk.Label(main_frame, text='エチロン')
+ech_text = tk.StringVar()
+echtext_entry = tk.Entry(main_frame, textvariable=ech_text)
 
-root.mainloop()
+create_btn = tk.Button(main_frame, text="作成", command=create)
+
+ship_label.grid(column=0, row=0, sticky=tk.W)
+shiptext_entry.grid(column=0, row=1, pady=5, sticky=tk.EW)
+
+ech_label.grid(column=0, row=3, sticky=tk.W)
+echtext_entry.grid(column=0, row=4, pady=5, sticky=tk.EW)
+
+create_btn.grid(column=0, row=5, pady=15)
+
+main_win.columnconfigure(0, weight=1)
+main_win.rowconfigure(0, weight=1)
+main_frame.columnconfigure(0, weight=1)
+
+main_win.mainloop()
