15f97551279eb4bb54a7fce2c14ec3f2a548476f
Masa H <masamasa0669@gmail.com>
2022-02-21 00:38:15 +0900

    Orderの全件自動作成成功

--- add_order.py
+++ add_order.py
@@ -9,19 +9,22 @@ orderlist = op.load_workbook('Order List.xlsx')
 ws1 = orderlist['RTN']
 value1 = [[cell.value for cell in row1] for row1 in ws1]
 
-#new_dir_filepath = 'test'
-#os.makedirs(new_dir_filepath)
+new_dir_filepath = 'test'
+os.makedirs(new_dir_filepath)
 
-word_file = glob.glob('*.docx')
-select_file = ','.join(word_file)
-doc = dx.Document(select_file)
-tbl = doc.tables[0]
-target = tbl.rows[3]
-for cell in target.cells:
-  cell_para = cell.paragraphs[0]
-  for run in cell_para.runs:
-    for i in range(len(value1)):
-      run.text = re.sub(value1[0][0], str(value1[i][0]), run.text)
-      print(value1[i][0])
-      #word_newFilePath = f'test/{value1[i][0]}.docx'
-      #doc.save(word_newFilePath)
+for i in range(1, len(value1)):
+  dic = dict(zip(value1[0], value1[i]))
+
+  word_file = glob.glob('*.docx')
+  select_file = ','.join(word_file)
+  doc = dx.Document(select_file)
+  tbl = doc.tables[0]
+  target = tbl.rows[3]
+  for cell in target.cells:
+    cell_para = cell.paragraphs[0]
+    for run in cell_para.runs:
+      for key, value in dic.items():
+        run.text = run.text.replace(key, str(value))
+
+      word_newFilePath = f'test/{i}_{value1[i][0]}.docx'
+      doc.save(word_newFilePath)
