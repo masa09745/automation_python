85d1ff5085552a3079a14f2e80e2880e1180a735
Masa H <masamasa0669@gmail.com>
2022-04-03 02:25:57 +0900

    Tkinterの部分切り出し、file c'k と auto print functionの修正

--- auto_print.py
+++ auto_print.py
@@ -2,30 +2,31 @@ import win32api
 import sys
 import os
 import time
-import glob
 from natsort import natsorted
 
 import tkinter as tk
 import tkinter.filedialog
 
 def auto_print(path):
-  win32api.shellExcute(0, "print", path, None, ".", 0)
-  print ("Printed:" + path)
-  
+  win32api.shellExecute(0, "print", path, None, ".", 0)
+ print ("Printed:" + path)
 
-def filepath():
-  dir = os.getcwd()
-  root = tk.Tk()
-  root.withdraw()
 
-  target_dir = tkinter.filedialog.askdirectory(initialdir = dir)
-  files = glob.glob(os.path.join(target_dir, '*.docx'))
-  sort_list = natsorted(files)
-  for filepath in sort_list:
-    auto_print(filepath)
+def file_check(path):
+  if os.path.isdir(path):
+    files = natsorted(os.listdir(path))
+    print(files)
+    for file in files:
+      file_check(path + "\\" + file)
+  else:
+    auto_print(path)
     time.sleep(3)
-    
 
-filepath ()
-auto_print()
+
+dir = os.getcwd()
+print_path = tkinter.filedialog.askdirectory(initialdir = dir)
+file_check(print_path)
+
+root = tk.Tk()
+root.withdraw()
 
